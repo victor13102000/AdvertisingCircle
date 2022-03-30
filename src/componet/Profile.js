b4f68633eb3642025cdb7471469fef44568e63f9
matiasrocca <matiasrocca98@gmail.com>
2022-03-30 18:52:37 -0300

    Avances en el front, agregado de algunas rutas

--- src/componet/Profile.js
+++ src/componet/Profile.js
@@ -1,6 +1,23 @@
-
+import axios from "axios";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { profileRegister } from "../service/registerFunction";
 
 function Profile(){
+
+    const { register, handleSubmit } = useForm();
+
+    useEffect(() => {
+        // Pedido axios pidiendo la informacion del perfil del usuario a la base de datos
+        // Guardamos esos datos para poder mostrarlos por defecto en el formulario;
+      }, []);
+
+    const onSubmit = (data, e) =>{
+        e.preventDefault()
+        // Pedido axios editando esta informacion en la base de datos
+        profileRegister(data)
+
+    }
 
 
     return (
@@ -8,51 +25,51 @@ function Profile(){
             <div className="contenedorPerfil">
                 <h2>Update your profile information</h2>
                 <div className="contenedorPerfilForm">
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <div class="form-row">
                             <div class="col">
                                 <label for="inputFirstName4">First Name</label>
-                                <input type="text" class="form-control"/>
+                                <input {... register("firstName")} type="text" class="form-control"/>
                             </div>
                             <div class="col">
                                 <label for="inputLastName4">Last Name</label>
-                                <input type="text" class="form-control"/>
+                                <input {... register("lastName")} type="text" class="form-control"/>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="inputCity">Language</label>
-                                    <input type="text" class="form-control" id="inputLanguage"/>
+                                <label for="inputLanguage">Language</label>
+                                    <input {... register("language")} type="text" class="form-control" id="inputLanguage"/>
                             </div>
                             <div class="form-group col-md-4">
-                                <label for="inputState">Gender:</label>
-                                    <select id="inputState" class="form-control">
+                                <label for="inputGender">Gender:</label>
+                                    <select {... register("gender")} id="inputGender" class="form-control">
                                         <option selected>Choose...</option>
                                         <option>Male</option>
                                         <option>Female</option>
                                     </select>
                             </div>
                             <div class="form-group col-md-2">
-                                <label for="inputZip">Age</label>
-                                    <input type="text" class="form-control" id="inputAge"/>
+                                <label for="inputAge">Age</label>
+                                    <input {... register("age")} type="number" class="form-control" id="inputAge"/>
                             </div>
                         </div>
                         <div class="form-row">
                             <div class="form-group col-md-3">
-                                <label for="inputEmail4">Instagram</label>
-                                    <input type="email" class="form-control" id="inputInstagram3"/>
+                                <label for="inputInstagram4">Instagram</label>
+                                    <input {... register("instagram")} type="url" class="form-control" id="inputInstagram3"/>
                             </div>
                             <div class="form-group col-md-3">
-                                <label for="inputEmail4">Tik Tok</label>
-                                    <input type="email" class="form-control" id="inputTikTok3"/>
+                                <label for="inputTikTok4">Tik Tok</label>
+                                    <input {... register("tikTok")} type="url" class="form-control" id="inputTikTok3"/>
                             </div>
                             <div class="form-group col-md-3">
-                                <label for="inputEmail4">Youtube</label>
-                                    <input type="email" class="form-control" id="inputYoutube3"/>
+                                <label for="inputYoutube4">Youtube</label>
+                                    <input {... register("youtube")} type="url" class="form-control" id="inputYoutube3"/>
                             </div>
                             <div class="form-group col-md-3">
-                                <label for="inputPassword4">Tweeter</label>
-                                    <input type="password" class="form-control" id="inputTweeter3"/>
+                                <label for="inputTwitter4">Twitter</label>
+                                    <input {... register("twitter")} type="url" class="form-control" id="inputTwitter3"/>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary">Update</button>
